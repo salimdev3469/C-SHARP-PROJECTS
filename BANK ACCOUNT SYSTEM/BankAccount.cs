dee927eb3e19fabaa323fc36a624e76eb9e654b2
salimdev3469 <157348323+salimdev3469@users.noreply.github.com>
2024-04-23 16:52:45 +0300

    simple bank account system

---
+++ BANK ACCOUNT SYSTEM/BankAccount.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANK_ACCOUNT_SYSTEM
+{
+    abstract class BankAccount
+    {
+       public long AccountNo { get; set; }
+        public string IBAN { get; set; }
+        public int BranchCode { get; set; }
+        public decimal Balance { get; set; }
+
+        public virtual string Withdraw(decimal amount)
+        {
+            Balance -= amount;
+            return amount + "$ withdrawed from your account. Initial balance: "+Balance;
+        }
+
+        public virtual string Deposit(decimal amount)
+        {
+            Balance += amount;
+            return amount + "$ deposited to your account.Initial balance: "+Balance;
+        }
+
+
+    }
+
+
+    class CurrentAccount : BankAccount
+    {
+        public override string Withdraw(decimal amount)
+        {
+            if (Balance < amount)
+            {
+                return "Insufficient Balance.";
+            }
+            if (amount%5 == 0){ //only 5 or multpiles of 5 are withdrawable.
+                return base.Withdraw(amount);//when you call with base class, it goes to inherited/parent class
+            }
+            else
+            {
+                return "You can withdraw number of 5 or multiples of 5.";
+            }
+        }
+
+    }
+
+    class SavingsAccount : BankAccount
+    {
+        public DateTime SavingBeginTime { get; set; }
+        public int numberOfSavingDays { get; set; }
+        public DateTime SavingEndTime {
+        get
+            {
+                return SavingBeginTime.AddDays(numberOfSavingDays);
+            }
+        }
+
+        public override string Withdraw(decimal amount)
+        {
+            if (DateTime.Today != SavingEndTime.Date)
+            {
+                return "Wait for the savings end date.";
+            }
+            else if(Balance<amount) {
+                return "Insufficient balance.";
+            }
+            else if(amount%5 != 0)
+            {
+                return "You can ";
+            }
+            else
+            {
+                return base.Withdraw(amount);
+            }
+        }
+
+        public override string Deposit(decimal amount)
+        {
+            if (DateTime.Today.Date == SavingEndTime.Date)
+            {
+                return base.Deposit(amount);
+            }
+            else
+            {
+                return "Wait for the savings end date";
+            }
+        }
+
+
+    }
+}
---
+++ BANK ACCOUNT SYSTEM/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace BANK_ACCOUNT_SYSTEM
+{
+    partial class Form1ANJ
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.Color.Gainsboro;
+            this.numericUpDown1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.numericUpDown1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.numericUpDown1.Location = new System.Drawing.Point(223, 86);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1215752192,
+            23,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(284, 22);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Montserrat", 8.25F);
+            this.button1.Location = new System.Drawing.Point(223, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "WITHDRAW";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Montserrat", 8.25F);
+            this.button2.Location = new System.Drawing.Point(223, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(284, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "DEPOSIT";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.8F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(175, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(416, 39);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "BANK ACCOUNT SYSTEM";
+            // 
+            // Form1ANJ
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::BANK_ACCOUNT_SYSTEM.Properties.Resources._105e5b90676eb46ad51a66a44582a573;
+            this.ClientSize = new System.Drawing.Size(728, 427);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.HelpButton = true;
+            this.Name = "Form1ANJ";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BANK ACCOUNT ACCESS WINDOW";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
